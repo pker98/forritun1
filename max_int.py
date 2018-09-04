37874e6919075b5aa784995e14d3a70af6755ba4
Matthías Finnur <matthias18@ru.is>
2018-09-04 10:08:38 +0000

    max_int er tilbuid

---
+++ max_int.py
@@ -0,0 +1,14 @@
+max_int = 0
+num_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    # Fill in the missing code
+    if num_int > max_int: 
+        max_int = num_int 
+    
+print("The maximum is", max_int)    # Do not change this line    
+    
+
+
+
